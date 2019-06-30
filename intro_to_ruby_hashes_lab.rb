eda258f6a63ca76f3e3c77e7e01ed1d43c4502ba
Randall Taylor <randyisbusy@gmail.com>
2019-06-30 18:41:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,6 +32,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  dude = Hash.new
+  dude[key] = value
+  return dude
 end
 
 def read_from_hash(hash, key)
